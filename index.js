e1f2f08eb5168fb4d39d0590c5e782f42efbee5b
shiwenwen <s13731984233@163.com>
2016-10-04 13:50:36 +0800

    bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
  * Created by sww on 2016/10/2.
  */
-import Toast from './lib/SActivityIndicator';
-export default Toast;
+import SActivityIndicator from './lib/SActivityIndicator';
+export default SActivityIndicator;
